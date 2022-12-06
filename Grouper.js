6170b082f5d3449438caa6b452c174e5282f0c07
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-12-06 21:33:58 +0100

    Added group calculation

--- Grouper.js
+++ Grouper.js
@@ -5,7 +5,7 @@ class Grouper {
 	}
 	
 	addIndividual() {
-		let individualIndex = this.individuals.length;
+		let individualIndex = this.individuals.length+1;
 		this.individuals.push(
 			{
 				name: "n"+individualIndex,
@@ -15,6 +15,34 @@ class Grouper {
 		return individualIndex;
 	}
 	
+	parseMarksForIndividual(markString, individualIndex) {
+		let marks = markString.split(" ");
+		this.individuals[individualIndex].marks = []
+		for (let i=0; i<marks.length; i++) {
+			this.individuals[individualIndex].marks.push(parseFloat(marks[i]))
+		}
+		let finalMarks = this.individuals[individualIndex].marks;
+		this.individuals[individualIndex].average = finalMarks.reduce((a, b) => a + b) / finalMarks.length;
+	}
+
+	getGroups() {
+		let groups = [];
+		this.individuals.sort((a, b) => parseFloat(a.average) - parseFloat(b.average));
+		let groupSize = document.getElementById("groupSizeField").value
+		let oddGroup = [], count = 0;
+		while (this.individuals.length % groupSize != 0) {
+			oddGroup.push(this.individuals[this.individuals.length/groupSize*count])
+			this.individuals.splice(this.individuals.length/groupSize*count, 1);
+		}
+		for (let i=0; i<this.individuals.length/groupSize; i++) {
+			let group = [];
+			for (let j=0; j<groupSize; j++) {
+				group.push(this.individuals[this.individuals.length/groupSize*j + i])
+			}
+			groups.push(group);
+		}
+		return groups;
+	}
 	
 }
 
@@ -24,4 +52,26 @@ function addIndividual() {
 	let individualIndex = grouper.addIndividual();
 	document.getElementById("individualList").innerHTML += 
 		"<h4 id='individual"+individualIndex+"'>n"+individualIndex+": </h4><input type='text' cols='25'>";
+	document.getElementById("getGroupsButton").disabled=false;
+}
+
+function calculateGroups() {
+	let individuals = document.getElementById("individualList").getElementsByTagName("input");
+	individuals = Array.from(individuals);
+	let count = 0;
+	individuals.forEach(individual => {
+		grouper.parseMarksForIndividual(individual.value.trim(), count);
+		count++;
+	});
+	printGroupsOnConsole(grouper.getGroups());
+}
+
+function printGroupsOnConsole(groups) {
+	document.getElementById("console").value = "Calculated groups: "+'\n';
+	groups = groups.map(group => group.map(individual => individual.name));
+	for (let i=0; i<groups.length; i++) {
+		let groupMembers = "";
+		groups[i].forEach(member => groupMembers += member+" ")
+		document.getElementById("console").value += "Group number "+(i+1)+": "+groupMembers+ '\n';
+	}
 }
